d96fec320b413f8f47b7c64f0160dd37c787eaa5
Jason Seril <jasonseril@gmail.com>
2019-01-02 23:03:28 +0800

    etc

--- functions.R
+++ functions.R
@@ -1,1 +1,33 @@
 ## functions in R
+
+## sample 1
+add2 <- function(x, y){
+  x + y
+}
+
+## 
+above10 <- function(x){
+  use <- x > 10
+  x[use]
+}
+
+above <- function(x, n=10){
+  use <- x > n
+  x[use]
+}
+
+## 
+columnmean <- function(y, removeNA= TRUE){
+  nc <- ncol(y)
+  means <- numeric(nc)
+  for(i in 1:nc){
+    mean[i] <- mean(y[,i], na.rm = removeNA)
+  }
+  means
+}
+
+## Argument Matching
+mydata <- rnorm(100)
+sd(mydata)
+sd(x = mydata)
+sd(x = mydata, na.rm = FALSE)
